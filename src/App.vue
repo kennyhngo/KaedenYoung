e22ee88f1d1e41c9989a5df3be89584cba4ba9dd
kennyhngo <kennyngodev@gmail.com>
2025-06-30 16:52:53 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Benefits from "./components/Benefits.vue";
+import Community from "./components/Community.vue";
+import Contact from "./components/Contact.vue";
+import FAQ from "./components/FAQ.vue";
+import Features from "./components/Features.vue";
+import Footer from "./components/Footer.vue";
+import Hero from "./components/Hero.vue";
+import HowItWorks from "./components/HowItWorks.vue";
+import Navbar from "./components/Navbar.vue";
+import Pricing from "./components/Pricing.vue";
+import Services from "./components/Services.vue";
+import Sponsors from "./components/Sponsors.vue";
+import Team from "./components/Team.vue";
+import Testimonials from "./components/Testimonials.vue";
+</script>
+
+<template>
+  <Navbar />
+  <Hero />
+  <Sponsors />
+  <Benefits />
+  <Features />
+  <Services />
+  <HowItWorks />
+  <Testimonials />
+  <Team />
+  <Community />
+  <Pricing />
+  <Contact />
+  <FAQ />
+  <Footer />
+</template>
