a021d746b3e1accad515a21ba1765d5e9e500cc3
kennyhngo <kennyngodev@gmail.com>
2025-07-04 02:27:11 -0400

    added base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
 export default defineConfig({
+  base: '/KaedenYoung/',
   css: {
     postcss: {
       plugins: [tailwind(), autoprefixer()],
